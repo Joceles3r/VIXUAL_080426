0aa5e1cddc15bead9b146772911404a93be73d74
Joceles3r <jocelyndru@gmail.com>
2026-06-12 23:40:43 +0200

    feat: ajout mini carrousel V1

---
+++ lib/v1-mini-carousel.ts
@@ -0,0 +1,22 @@
+export const V1_MINI_CAROUSEL = [
+  {
+    title: "Films",
+    thumbnail: "/uploads/homepage/v1-mini-carousel/thumb-1.webp",
+  },
+  {
+    title: "Clips Vidéo",
+    thumbnail: "/uploads/homepage/v1-mini-carousel/thumb-2.webp",
+  },
+  {
+    title: "Documentaires",
+    thumbnail: "/uploads/homepage/v1-mini-carousel/thumb-3.webp",
+  },
+  {
+    title: "Podcasts",
+    thumbnail: "/uploads/homepage/v1-mini-carousel/thumb-4.webp",
+  },
+  {
+    title: "Livres",
+    thumbnail: "/uploads/homepage/v1-mini-carousel/thumb-5.webp",
+  },
+]
